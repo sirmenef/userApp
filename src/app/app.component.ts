57ef091d34ab6c52093b818c514610b96886bede
Sirmenef <wearysamm@gmail.com>
2020-06-21 14:56:55 +0100

    ii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { UserServiceService } from './services/user-service.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'userApp';
+export class AppComponent implements OnInit {
+
+  constructor(private api: UserServiceService) {
+
+  }
+
+  currentPage = 1;
+  resultsPerPage = 3;
+  users: any;
+  maxPage = 7;
+
+  pageConfig = {
+    page: String(this.currentPage),
+    results: String(this.resultsPerPage),
+    seed: 'sammy',
+  };
+
+  ngOnInit() {
+    this.loadUsers(this.pageConfig);
+  }
+
+
+
+  loadUsers(Config) {
+
+    this.api.loadUsers(Config).subscribe(
+      val => {
+        if (val) {
+          this.users = val.results;
+        }
+        console.log(val.results);
+      }
+    );
+  }
+
+  getUsers(user: any) {
+
+  }
+
+  downloadUsers() {
+    this.api.downloadUsers({ results: '3', seed: 'sammy', format: `csv&dl` }).subscribe();
+  }
+
+  getMaleUsers() {
+
+  }
+
+  getFemaleUsers() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { CardComponent } from './card/card.component';
+import { TestComponent } from './test/test.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDetailsComponent,
+    CardComponent,
+    TestComponent,
+    SidebarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
