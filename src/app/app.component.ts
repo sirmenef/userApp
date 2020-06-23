d24b77f2597b0e6f2f8d6f3923fe7cf47038f189
Sirmenef <wearysamm@gmail.com>
2020-06-23 17:29:04 +0100

    major update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {UserServiceService} from './services/user-service.service';
 import {Component, OnInit} from '@angular/core';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,49 +14,258 @@ export class AppComponent implements OnInit {
 
   allUsers: object[] = [];
   isCountry: boolean;
+  maleUsers: object[];
+  femaleUser: object[];
+  filterByOptions: string[] = ['Country', 'Name'];
+  pagetitle = 'All Users';
+  currentView = 'all users';
+  filterOption: string;
+  filteredUsers: object[];
+
   currentPage = 1;
   resultsPerPage = 3;
   users: any;
   maxPage = 7;
   CountryList = [];
   pageConfig = {
-    page: String(this.currentPage),
-    results: String(this.resultsPerPage),
+    currentPage: 1,
+    resultsPerPage: 3,
+    maxPage: 7,
+    page() {
+      return this.currentPage.toString();
+    },
+    results() {
+      return this.resultsPerPage.toString();
+    },
     seed: 'sammy',
+  };
+  maleConfig = {
+    currentPage: 1,
+    resultsPerPage: 3,
+    maxPage: 4,
+    start: 0,
+    end: 3,
+  };
+  femaleConfig = {
+    currentPage: 1,
+    resultsPerPage: 3,
+    maxPage: 3,
+    start: 0,
+    end: 3,
+  };
+  filterConfig = {
+    currentPage: 1,
+    resultsPerPage: 3,
+    maxPage: 0,
+    start: 0,
+    end: 3,
   };
   viewUser: object;
   nav: string;
 
   ngOnInit() {
     this.loadUsers(this.pageConfig);
-    // this.getAllUsers();
+    this.getAllUsers();
     // this.getCountry();
     this.showCountry();
     // console.log(this.allUsers);
     // this.log();
   }
 
-  async loadUsers(Config) {
-    await this.api.loadUsers(Config).subscribe(
+  loadUsers(Config) {
+    this.api.loadUsers(Config).subscribe(
       val => {
         if (val) {
           this.users = val.results;
         }
-        // console.log(val.results);
         console.log('users');
         console.log(this.users);
       }
     );
   }
 
-  search(name) {
-    // tslint:disable-next-line:no-unused-expression
-    this.api.loadUsers({page: '1', seed: 'sammy', results: '20'}).subscribe(res => {
-      this.users = res.results.filter(x => x['name']['first'].include(name) || x['name']['last'].include(name)
-      );
+  setUserView(type: string) {
+    switch (type) {
+      case 'all users':
+        this.loadUsers(this.pageConfig);
+        this.currentView = 'all users';
+        break;
+      case 'male':
+        const maleUser = this.allUsers.filter(x => x['gender'] === 'male');
+        if (maleUser.length > 0) {
+          this.users = maleUser.slice(0, 3);
+          this.maleUsers = maleUser;
+          this.currentView = 'male';
+        }
+        break;
+      case 'female':
+        const femaleUser = this.allUsers.filter(x => x['gender'] === 'female');
+        if (femaleUser.length > 0   ) {
+          this.users = femaleUser.slice(0, 3);
+          this.femaleUser = femaleUser;
+          this.currentView = 'female';
+        }
+        break;
+    }
+  }
+
+  getAllUsers() {
+    this.api.loadUsers({page() {return '1'; }, results() {return '20'; }, seed: 'sammy'}).subscribe(res => {
+      if (res) {
+        this.allUsers = res.results;
+      }
     });
   }
 
+  search(name: string) {
+    const val =  name['0'].toUpperCase() + name.slice(1);
+    const indexOfSpace: number = name.indexOf(' ');
+    if (indexOfSpace > 1) { name[indexOfSpace + 1].toUpperCase(); }
+    this.api.loadUsers({page() {return '1'; }, seed: 'sammy', results() { return '20'; } }).subscribe(res => {
+      this.users = res.results.filter(x => x['name']['first'].includes(val) || x['name']['last'].includes(val));
+    });
+  }
+
+  filterBy(options: string) {
+    this.filterOption = options;
+  }
+
+  // @ts-ignore
+  filterUsers(input: string) {
+    const val =  input['0'].toUpperCase() + input.slice(1);
+    if (this.filterOption === 'Country') {
+      this.filteredUsers = this.allUsers.filter(x => x['location']['country'].includes(input));
+      this.users = this.filteredUsers.slice(0, 3);
+    } else {
+      this.filteredUsers = this.allUsers.filter(x => x['name']['first'].includes(val) || x['name']['last'].includes(val));
+      this.users = this.filteredUsers.slice(0, 3);
+    }
+    const length = this.filteredUsers.length;
+    if (this.filteredUsers.length > 3) {
+      this.filterConfig.maxPage = ((length % 3) === 0) ? (length / 3) : ((length / 3) + 1);
+    } else { this.filterConfig.maxPage = 1; }
+  }
+
+  showCountry() {
+    this.isCountry = !this.isCountry;
+  }
+
+  navigate(input: string) {
+   switch (input) {
+     case 'next':
+       switch (this.currentView) {
+         case 'all users':
+           if (this.pageConfig.currentPage > this.pageConfig.maxPage) { return; }
+           if (this.pageConfig.currentPage === this.pageConfig.maxPage) {
+              this.pageConfig.resultsPerPage = 2;
+              this.loadUsers(this.pageConfig);
+           } else if (this.maleConfig.currentPage > 0) {
+              this.pageConfig.resultsPerPage = 3;
+              this.pageConfig.currentPage++;
+              this.loadUsers(this.pageConfig);
+           }
+           this.maleConfig.currentPage = 1;
+           this.femaleConfig.currentPage = 1;
+           this.filterConfig.currentPage = 1;
+           break;
+         case 'male':
+           if (this.maleConfig.currentPage > this.maleConfig.maxPage) { return; }
+           if (this.maleConfig.currentPage === this.maleConfig.maxPage) {
+             this.users = this.maleUsers.slice(9);
+           } else if (this.maleConfig.currentPage > 0) {
+             this.maleConfig.start += 3;
+             this.maleConfig.end += 3;
+             this.maleConfig.currentPage++;
+             this.users = this.maleUsers.slice(this.maleConfig.start, this.maleConfig.end);
+           }
+           this.pageConfig.currentPage = 1;
+           this.femaleConfig.currentPage = 1;
+           this.filterConfig.currentPage = 1;
+           break;
+         case 'female':
+           if (this.femaleConfig.currentPage > this.femaleConfig.maxPage) { return; }
+           else if (this.femaleConfig.currentPage === this.femaleConfig.maxPage) {
+             this.users = this.femaleUser.slice(this.femaleConfig.start);
+           } else if (this.femaleConfig.currentPage > 0) {
+             this.femaleConfig.currentPage++;
+             this.femaleConfig.start += 3;
+             this.femaleConfig.end += 3;
+             this.users = this.femaleUser.slice(this.femaleConfig.start, this.femaleConfig.end);
+           }
+           break;
+           this.maleConfig.currentPage = 1;
+           this.pageConfig.currentPage = 1;
+           this.filterConfig.currentPage = 1;
+         default:
+           if (this.filterConfig.currentPage > this.filterConfig.maxPage) { return; }
+           else if (this.filterConfig.currentPage === this.filterConfig.maxPage) {
+             this.users = this.maleUsers.slice(this.femaleConfig.start);
+           } else if (this.filterConfig.currentPage > 0) {
+             this.femaleConfig.start += 3;
+             this.femaleConfig.end += 3;
+             this.femaleConfig.currentPage++;
+             this.users = this.femaleUser.slice(this.femaleConfig.start, this.femaleConfig.end);
+           }
+           this.maleConfig.currentPage = 1;
+           this.femaleConfig.currentPage = 1;
+           this.pageConfig.currentPage = 1;
+       }
+       break;
+     case 'prev':
+       switch (this.currentView) {
+         case 'all users':
+           if (this.pageConfig.currentPage === 1) {
+             return;
+           }
+           if (this.pageConfig.currentPage === this.pageConfig.maxPage) {
+             this.pageConfig.resultsPerPage = 2;
+             this.loadUsers(this.pageConfig);
+           } else if (this.pageConfig.currentPage < this.pageConfig.maxPage) {
+             this.pageConfig.resultsPerPage = 3;
+             this.pageConfig.currentPage--;
+             this.loadUsers(this.pageConfig);
+           }
+           this.maleConfig.currentPage = 1;
+           this.femaleConfig.currentPage = 1;
+           this.filterConfig.currentPage = 1;
+           break;
+         case 'male':
+           if (this.maleConfig.currentPage === 1) {
+             return;
+           }
+           if (this.maleConfig.currentPage === this.maleConfig.maxPage) {
+             this.users = this.maleUsers.slice(9);
+           } else if (this.maleConfig.currentPage < this.pageConfig.maxPage) {
+             this.maleConfig.start -= 3;
+             this.maleConfig.end -= 3;
+             this.users = this.maleUsers.slice(this.maleConfig.start, this.maleConfig.end);
+             this.maleConfig.currentPage--;
+           }
+           this.pageConfig.currentPage = 1;
+           this.femaleConfig.currentPage = 1;
+           this.filterConfig.currentPage = 1;
+           break;
+         case 'female':
+           if (this.femaleConfig.currentPage === 1) {
+             return;
+           } else if (this.femaleConfig.currentPage > 1) {
+             this.femaleConfig.start -= 3;
+             this.femaleConfig.end -= 3;
+             this.users = this.femaleUser.slice(this.femaleConfig.start, this.femaleConfig.end);
+             this.femaleConfig.currentPage--;
+           }
+           this.maleConfig.currentPage = 1;
+           this.pageConfig.currentPage = 1;
+           this.filterConfig.currentPage = 1;
+           break;
+       }
+       break;
+   }
+  }
+
+  checkUser(user: object) {
+
+  }
   // getCountry() {
   //   this.allUsers.forEach(user => {
   //     const country = user['location']['country'];
@@ -71,29 +281,5 @@ export class AppComponent implements OnInit {
     // object = this.allUsers.find(x => x === x['name']['first'] || x['name']['last'] || `${x['name']['first']} ${x['name']['last']}`
     // );
     // this.viewUser = object;
-  }
-
-  showCountry() {
-   this.isCountry = !this.isCountry;
-  }
-
-  searchUser(input: string) {
-    let names: any;
-    this.api.loadUsers({page: '1', seed: 'sammy', results: '20'}).subscribe(
-      val => {
-        names = val.results;
-      });
-  }
-
-  async getAllUsers() {
-    await this.api.loadUsers({page: '1', results: '20', seed: 'sammy'}).subscribe(res => {
-      if (res) {
-        this.allUsers = res.results;
-      }
-    });
-  }
-
-  navigateUser(event) {
-    this.users = event;
   }
 }
